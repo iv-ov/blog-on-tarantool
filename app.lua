d2355098f774012aeee3a2fa8f4ad1463e6d3b9e
IvOv (mac) <iv-ov@users.noreply.github.com>
2019-10-12 15:43:43 +0300

    Migrated to React. Added pagination. Now API is availabe form different origin (and now frontend runs on different origin — port :3000 vs :8080 [by default]).

--- app.lua
+++ app.lua
@@ -12,27 +12,77 @@ if not box.space.blog then
 end
 -----
 
+
+local json = require('json')
+
 function addItemHandler(req)
     local data = req:json()
     local result = box.space.blog:auto_increment{
         data.title, data.text, os.time()
     }
-    return req:render({json = result})
+    return {
+        status = 201,
+        headers = {
+            -- @todo: carefully consider the right value for production. Now we allow any site to access the API
+            ['Access-Control-Allow-Origin'] = '*'
+        },
+        body = json.encode(result)
+    }
 end
 
-function getListHandler(req) return req:render({json = box.space.blog:select()}) end
+
+function getListHandler(req)
+    local page = tonumber(req:query_param('page')) or 1
+
+    local itemsPerPage = 2
+    local offset = itemsPerPage * (page - 1)
+
+    local items = box.space.blog:select({}, {offset = offset, limit = itemsPerPage})
+    local totalCount = box.space.blog:len()
+    return {
+        status = 200,
+        headers = {
+            -- @todo: carefully consider the right value for production. Now we allow any site to access the API
+            ['Access-Control-Allow-Origin'] = '*'
+        },
+        body = json.encode({
+            items = items,
+            totalPages = math.ceil(totalCount / itemsPerPage)
+        })
+    }
+end
 
 -- Обобщённый обработчик манипуляции над конкретным постом — удаления и редактирования
 function itemManipulationHandler(req)
     local id = tonumber(req:stash('id'))
 
     local result = 'Hmm...';
+
     if req.method == 'DELETE' then
         result = delete(id)
+        return {
+            status = 200,
+            headers = {
+                -- @todo: carefully consider the right value for production. Now we allow any site to access the API
+                ['Access-Control-Allow-Origin'] = '*'
+            },
+            body = json.encode(result)
+        }
     end
 
     if req.method == 'PUT' then
         result = update(id, req:post_param())
+    end
+
+    if req.method == 'OPTIONS' then
+        return {
+            status = 200,
+            headers = {
+                -- @todo: carefully consider the right value for production. Now we allow any site to access the API
+                ['Access-Control-Allow-Origin'] = '*',
+                ['Access-Control-Allow-Methods'] = 'PUT, DELETE, GET, OPTIONS'
+            }
+        }
     end
 
     return req:render({json = result})
--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Posts from './Posts';
+import Pagination from './Pagination';
+import PostForm from './PostForm';
+
+
+const API_URL = 'http://localhost:8080';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState({items:[], totalPages: 0});
+    const [page, setPage] = useState(1);
+
+    const [submitAllowed, setSubmitAllowed] = useState(true);
+
+    // Just an indicator that the data is changed by the user and we need to download the latest data
+    const [lastUpdated, setLastUpdated] = useState(0);
+    const signalizeChanges = () => setLastUpdated(Date.now());
+
+    // Load posts when page number or data changes (user created or deleted a post)
+    useEffect(
+        () => {
+            fetch(`${API_URL}/posts?page=${page}`)
+                .then(response => response.json())
+                .then(data => {
+                    setData(data);
+                }).catch(error => {
+                    console.log(error);
+                });
+        },
+        [page, lastUpdated],
+    );
+
+    const formRef = React.createRef();
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setSubmitAllowed(false);
+
+        const data = {};
+        //@TODO we wanted to be quick and dirty, fix this
+        const controls = document.querySelectorAll('input, textarea');
+        Array.from(controls).forEach(el => {
+            data[el.getAttribute('name')] = el.value;
+        });
+
+        const formNode = formRef.current;
+
+        fetch(`${API_URL}/posts/create`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+        }).then(function (_response) {
+            formNode.reset();
+            setSubmitAllowed(true);
+            signalizeChanges();
+        }).catch(function (error) {
+            setSubmitAllowed(true);
+            console.log(error);
+        })
+    };
+
+    function deletePost(id) {
+        fetch(`${API_URL}/posts/${id}`, {
+            method: 'DELETE'
+        }).then(function (_response) {
+            signalizeChanges();
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    return (
+        <div className="row">
+            <div className="col">
+                <h2>Add post</h2>
+                <PostForm {...{formRef, onSubmit, submitAllowed}} />
+            </div>
+            <div className="col">
+                <h2>Posts</h2>
+                {data.totalPages ? <Pagination {...{page, setPage, totalPages: data.totalPages}} /> : null}
+                <Posts {...{posts: data.items, deletePost}} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
