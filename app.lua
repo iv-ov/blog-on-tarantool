d4e98d6926d05b028e58b1ca69b60dc05dea09f8
IvOv (mac) <iv-ov@users.noreply.github.com>
2019-10-14 02:49:46 +0300

    Changed iterator type to "REQ" (instead of "LE")

--- app.lua
+++ app.lua
@@ -51,7 +51,7 @@ function getListHandler(req)
     local itemsPerPage = 2
     local offset = itemsPerPage * (page - 1)
 
-    local items = box.space.blog:select({}, {iterator = 'LE', offset = offset, limit = itemsPerPage})
+    local items = box.space.blog:select({}, {iterator = 'REQ', offset = offset, limit = itemsPerPage})
     local totalCount = box.space.blog:len()
     return {
         status = 200,
